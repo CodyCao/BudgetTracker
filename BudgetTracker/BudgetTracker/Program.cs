6095233bd28d130be48cf2f5d578240d214e32b4
Cody Cao <cody.cao26@gmail.com>
2020-01-15 15:32:44 -0800

    First Commit with VS project

---
+++ BudgetTracker/BudgetTracker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
